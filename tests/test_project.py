0ad15ce097a694222f984b40ae648af1f8343e02
acpla <aclpa35@gmail.com>
2026-01-18 21:22:17 +0900

    kanban 드래그 엔 드롭 기능에 api로 이슈 생성

--- tests/test_project.py
+++ tests/test_project.py
@@ -107,21 +107,22 @@ def test_create_issue(driver, fake, new_project): # tc_09
 
     print(f"✅ 이슈 생성 확인 완료: {random_issue_title}")
 
-def test_kanban(driver): # tc_10
+def test_kanban(driver,new_issue): # tc_10
     print("\n[TC-10] 칸반 보드 테스트")
     
     # 1. 로그인
     login_page = LoginPage(driver)
     login_page.main_login(Config.TEST_EMAIL, Config.TEST_PASSWORD)
 
-    # 2. 'Projects' 메뉴로 이동 (DashboardPage 담당)
+    # 2. 'Kanban' 메뉴로 이동 (DashboardPage 담당)
     dashboard = DashboardPage(driver)
     dashboard.go_to_kanban() 
 
+    issue_title = new_issue['title']
     kanban_page = KanbanPage(driver)
     
     # 드래그 앤 드롭 수행
-    kanban_page.move_card_to_done()
+    kanban_page.move_card_to_in_progress(issue_title)
     
     # 검증: 카드가 진짜로 In Progress 컬럼 안에 있는지 확인
-    assert kanban_page.is_text_visible_in_column("test", "In Progress")
+    assert kanban_page.is_text_visible_in_column(issue_title, "In Progress")
